feeda216bac09e0e90784bc7660406d727c60b6e
alekseipeikhel <alekseipeikhel@gmail.com>
2024-06-09 06:25:48 +0300

    добавлен сервис нотификации задач и метод для получения из бд данных

--- src/main/java/pro/sky/telegrambot/repository/NotificationTaskRepository.java
+++ src/main/java/pro/sky/telegrambot/repository/NotificationTaskRepository.java
@@ -1,9 +1,21 @@
 package pro.sky.telegrambot.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import pro.sky.telegrambot.model.NotificationTask;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+
 @Repository
 public interface NotificationTaskRepository extends JpaRepository<NotificationTask, Long> {
+    // получить весь список нотификаций по дате и времени
+    @Query(value = "select * from notification_task nt " +
+            "where nt.notification_date = :localDate " +
+            "and nt.notification_time = :localTime", nativeQuery = true)
+    List<NotificationTask> getByDateAndTimeNow(@Param("localDate") LocalDate localDate,
+                                               @Param("localTime") LocalTime localTime);
 }
---
+++ src/main/java/pro/sky/telegrambot/service/NotificationReminderService.java
@@ -0,0 +1,50 @@
+package pro.sky.telegrambot.service;
+
+import com.pengrad.telegrambot.TelegramBot;
+import com.pengrad.telegrambot.request.SendMessage;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+import pro.sky.telegrambot.model.NotificationTask;
+import pro.sky.telegrambot.repository.NotificationTaskRepository;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
+
+@Service
+public class NotificationReminderService {
+    final private TelegramBot bot;
+    final private NotificationTaskRepository repository;
+
+    public NotificationReminderService(TelegramBot bot, NotificationTaskRepository repository) {
+        this.bot = bot;
+        this.repository = repository;
+    }
+    // запуск метода каждую минуту
+    @Scheduled(cron = "0 0/1 * * * *")
+    public void getNotificationTasksWhereIsTime() {
+        // получить текущее дату и врем до минут
+        LocalDateTime localDateTime = LocalDateTime.now().truncatedTo(ChronoUnit.MINUTES);
+        LocalDate localDate = localDateTime.toLocalDate();
+        LocalTime localTime = localDateTime.toLocalTime();
+        // получаем список нотификаций
+        List<NotificationTask> tasks = repository.getByDateAndTimeNow(localDate, localTime);
+        // если не пустой, то отправить нотификацию
+        if (!tasks.isEmpty()) {
+            sendNotificationTask(tasks);
+        }
+    }
+
+    public void sendNotificationTask(List<NotificationTask> tasks) {
+        // для каждой нотификации в списке
+        // параллельно отправляем сообщением в чат
+        tasks.stream()
+                .parallel()
+                .forEach(t -> {
+                    SendMessage sendMessage = new SendMessage(t.getChat_id(), "Напоминаю:\n" + t.getTask());
+                    bot.execute(sendMessage);
+                });
+    }
+}
