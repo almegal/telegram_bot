d0ec3a846a3179ed4df8cd3781c3cc934c6d432c
alekseipeikhel <alekseipeikhel@gmail.com>
2024-06-14 22:40:39 +0300

    изменение структуры бд

--- src/main/resources/changelog/change-db-0_1.sql
+++ src/main/resources/changelog/change-db-0_1.sql
@@ -1,12 +1,11 @@
 -- liquibase formatted sql
 
 -- changeset alexey:1
-CREATE TABLE notification_task (
+create TABLE notification_task (
     id SERIAL PRIMARY KEY,
     chat_id BIGINT NOT NULL,
     is_active BOOLEAN NOT NULL DEFAULT TRUE,
-    notification_time TIME NOT NULL,
-    notification_date DATE NOT NULL,
+    notification_date_time TIMESTAMP NOT NULL,
     created_at DATE NOT NULL,
     task VARCHAR(255) NOT NULL
 )
